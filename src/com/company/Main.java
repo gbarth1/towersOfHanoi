a335af0e1de417ae4ac9b5d368b7cdfe02ee7db0
gbarth1 <grace.m.barth@gmail.com>
2017-01-18 13:45:09 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Tower of Hanoi Game Instructions :)");
+        System.out.println("This program will solve this puzzle for you.");
+        System.out.println("Please input the number of disks to solve for:");
+        System.out.println("Instructions:");
+
+
+
+    }
+}
---
+++ src/com/company/TowersOfHanoi.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.*;
+public class TowersOfHanoi
+{
+    Scanner ans = new Scanner(System.in);
+
+    public int tower1;
+    public int tower2;
+    public int tower3;
+
+    public void TowersOfHanoi()
+    {
+        int diskNum = ans.nextInt();
+    }
+
+    public String solve(int disks)
+    {
+        tower1 = disks ;
+        tower2 = 0;
+        tower3 = 0;
+
+        if(tower2)
+
+
+    }
+}
